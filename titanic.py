3392e9be84ebb3bc40a97e32c0443b4b6c7e927c
Alex <114019241+edawhite@users.noreply.github.com>
2023-03-21 19:05:49 -0400

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -2,4 +2,25 @@ import numpy as np
 import pandas as pd
 import os
 
-train_data = pd.read_csv(train.csv)
+#Read the Training Data and Testing Data
+train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
+
+#Import the model
+from sklearn.ensemble import RandomForestClassifier
+
+#Define the parameters of the model
+y = train_data["Survived"]
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.fet_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+#Setting up number of trees in the forest
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+#Creating output and print statement when done
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
