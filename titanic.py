de8a250a28e99e6d735454e2315adc8749d2d60e
Alex <114019241+edawhite@users.noreply.github.com>
2023-03-27 12:48:19 -0400

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -6,8 +6,9 @@ import os
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
-#Import the model
+#Import the model and metrics
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 #Define the parameters of the model
 y = train_data["Survived"]
@@ -16,9 +17,12 @@ X = pd.fet_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
 
 #Setting up number of trees in the forest
+#Then create model and test for accuracy
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(X, y)
 predictions = model.predict(X_test)
+accuracy_score(y, predictions, normalize=False)
+print(classification_report(y,predictions))
 
 #Creating output and print statement when done
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
