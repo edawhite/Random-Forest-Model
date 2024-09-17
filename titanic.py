44d95f33bff35d041a66ee144a5020ef6cc87c8c
edawhite <edwardwhite92@gmail.com>
2024-09-17 17:07:38 -0500

    changed py file

---
+++ titanic.py
@@ -0,0 +1,61 @@
+import time
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
+
+#print(train_data.head())
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+
+#print("% of women who survived:", rate_women)
+
+men = train_data.loc[train_data.Sex == 'male']["Survived"]
+rate_men = sum(men)/len(men)
+
+#print("% of men who survived:", rate_men)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+y = train_data["Survived"]
+y_true = test_data["Survived"]
+print(y_true)
+#y_true = test_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+print(X)
+feature_names = [f"feature {i}" for i in range(X.shape[1])]
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+#accuracy_score(y,predictions, normalize=False)
+#print(classification_report(y_true,predictions))
+
+#output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+#output.to_csv('submission.csv', index=False)
+#print("Your submission was successfully saved!")
+
+start_time = time.time()
+importances = model.feature_importances_
+std = np.std([model.feature_importances_ for tree in model.estimators_], axis=0)
+elapsed_time = time.time() - start_time
+
+print(f"Elapsed time to compute the importances: {elapsed_time:.3f} seconds")
+
+model_importances = pd.Series(importances, index=feature_names)
+
+fig, ax = plt.subplots()
+model_importances.plot.bar(yerr=std, ax=ax)
+ax.set_title("Feature importances using MDI")
+ax.set_ylabel("Mean decrease in impurity")
+fig.tight_layout()
+plt.show()
+
+print("Checking for push update")
